da7a5fe4743d247ae959c4170a169f07e74ad314
amadouba211019 <amadouba211019@gmail.com>
2026-09-25 16:53:29 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const themeBtn = document.getElementById("themeBtn");
-const saved = localStorage.getItem("theme");
-if (saved === "dark") document.body.classList.add("dark");
-
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
-});
